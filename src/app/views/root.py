fca97cf6a73f871d7a7054d94c0bcdcbe4b7fa53
Kirill Zhosul <kirill_zhosul@vk.com>
2022-02-07 21:48:02 +0400

    About, contacts

--- src/app/views/root.py
+++ src/app/views/root.py
@@ -11,3 +11,13 @@ bp_root = Blueprint("root", __name__)
 @bp_root.route("/", methods=["GET"])
 def root_index():
     return render_template("index.jinja")
+
+
+@bp_root.route("/about", methods=["GET"])
+def root_about():
+    return render_template("about.jinja")
+
+
+@bp_root.route("/contacts", methods=["GET"])
+def root_contacts():
+    return render_template("contacts.jinja")
