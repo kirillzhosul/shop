3c1c26bce168509cd48e52dd251e926df5693788
Kirill Zhosul <kirill_zhosul@vk.com>
2022-02-09 21:45:23 +0400

    Update config

--- src/run.py
+++ src/run.py
@@ -7,5 +7,5 @@
 import app
 
 if __name__ == "__main__":
-    wsgi_app = app.create(None, app.config.ConfigProduction)
+    wsgi_app = app.create(None, app.config.ConfigDevelopment)
     wsgi_app.run(host="0.0.0.0", port=80)
