3e5c0444a3244ec49946d571abeba9d9b8a77cd9
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-02-18 13:16:00 +0400

    Registration

--- src/app/static/js/auth.js
+++ src/app/static/js/auth.js
@@ -5,3 +5,26 @@ function authLogin(email, password){
         }
     });
 }
+
+function authRegister(name, phone, email, password, passwordConfirmation){
+    var params = "";
+    params += "name=" + name.toString();
+    params += "&phone=" + phone.toString();
+    params += "&email=" + email.toString();
+    params += "&password=" + password.toString();
+    params += "&password_confirmation=" + passwordConfirmation.toString();
+
+    requestApi("auth/register", params, function(data, status){
+        if ("redirect_to" in data){
+            window.location = data["redirect_to"];
+        }
+    });
+}
+
+function authLogout(){
+    requestApi("auth/logout", "", function(data, status){
+        if ("redirect_to" in data){
+            window.location = data["redirect_to"];
+        }
+    });
+}
