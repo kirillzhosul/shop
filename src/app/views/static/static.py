b16e65fc3dacae6ba6c6d2005a47a6c364e1b1fb
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-02-24 14:43:10 +0400

    Move favicon inside template, not Flask view.

--- src/app/views/static/static.py
+++ src/app/views/static/static.py
@@ -10,15 +10,17 @@ bp_static = Blueprint("ext_static", __name__)
 
 @bp_static.route("/robots.txt")
 def robots():
+    """
+    Handles static `robots.txt` file for search index.
+    File should be located in the root, so this view does that.
+    """
     return send_from_directory(current_app.static_folder, "robots.txt")
 
 
 @bp_static.route("/sitemap.xml")
 def sitemap():
+    """
+    Handles static `sitemap.xml` file for search index.
+    File should be located in the root, so this view does that.
+    """
     return send_from_directory(current_app.static_folder, "sitemap.xml")
-
-
-@bp_static.route("/favicon.ico")
-def favicon():
-    return send_from_directory(current_app.static_folder, "favicon.svg")
-
